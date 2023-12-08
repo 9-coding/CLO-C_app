ca34577e506e2602ccb4bd53eebff83c99d1732e
9-coding <9.coding.lab@gmail.com>
2023-12-08 16:11:35 +0900

    error

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -7,16 +7,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import axios from 'axios';
 import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD7jlUzKiSs6oLOMptBnweP8XhrOuiUyZ8",
-    authDomain: "cloc-bdf74.firebaseapp.com",
-    databaseURL: "https://cloc-bdf74-default-rtdb.firebaseio.com/",
-    projectId: "cloc-bdf74",
-    storageBucket: "cloc-bdf74.appspot.com",
-    messagingSenderId: "485093561661",
-    appId: "1:485093561661:web:e4d4743dda2407b90f2154",
-    measurementId: "G-ZXG5FLMMFN"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
